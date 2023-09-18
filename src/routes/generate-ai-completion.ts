8e984ddd63678c5b06c4b51116fe201ff39c991f
Renan Costa Alencar <renan.costaalencar@gmail.com>
2023-09-18 00:35:21 -0300

    feat: Add integration to OpenAI API

--- src/routes/generate-ai-completion.ts
+++ src/routes/generate-ai-completion.ts
@@ -3,16 +3,17 @@ import { prisma } from "../lib/prisma";
 import { z } from "zod";
 import { createReadStream } from "node:fs";
 import { openai } from "../lib/openai";
+import { streamToResponse, OpenAIStream } from "ai";
 
 export async function generateAICompletionRoute(app: FastifyInstance) {
     app.post('/ai/complete', async (req, reply) => {
         const bodySchema = z.object({
             videoId: z.string().uuid(),
-            template: z.string(),
+            prompt: z.string(),
             temperature: z.number().min(0).max(1).default(0.5),
         });
 
-        const { videoId, template, temperature } = bodySchema.parse(req.body);
+        const { videoId, prompt, temperature } = bodySchema.parse(req.body);
 
         const video = await prisma.video.findUniqueOrThrow({
             where: {
@@ -24,7 +25,7 @@ export async function generateAICompletionRoute(app: FastifyInstance) {
             return reply.status(400).send({error: 'Video transcription wast not generated yet.'});
         }
 
-        const promptMessage = template.replace('{transcription}', video.transcription);
+        const promptMessage = prompt.replace('{transcription}', video.transcription);
 
         const response = await openai.chat.completions.create({
             model: 'gpt-3.5-turbo-16k',
@@ -32,9 +33,18 @@ export async function generateAICompletionRoute(app: FastifyInstance) {
             messages: [
                 {role: 'user', content: promptMessage},
             ],
+            stream: true,
+
         });
 
-        return response;
+        const stream = OpenAIStream(response);
+
+        streamToResponse(stream, reply.raw, {
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+            },
+        });
 
     });
 
